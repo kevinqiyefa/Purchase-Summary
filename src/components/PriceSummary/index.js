8c78f0f704c038ffc2c438cfd2323a1e2199ab37
Kevin <kevinqiyf@hotmail.com>
2019-03-25 16:14:34 -0700

    added loading spin for asyn request

--- src/components/PriceSummary/index.js
+++ src/components/PriceSummary/index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css';
 
-class PriceSummary extends Component {
-  render() {
-    return <div className="Price-Summary-Main">Price Summary</div>;
-  }
-}
+const PriceSummary = ({ pricing }) => {
+  // console.log(pricing.subtotal, 'ss');
+  return (
+    <div className="Price-Summary-Main">
+      <div>{pricing.subtotal}</div>
+    </div>
+  );
+};
 
 export default PriceSummary;
--- src/components/PurchaseSummary/index.js
+++ src/components/PurchaseSummary/index.js
@@ -9,12 +9,14 @@ class PurchaseSummary extends Component {
   render() {
     const { pricing, itemDetails } = this.props.pricingData;
 
-    return (
-      <div className="Summary-Container-Main">
+    return pricing ? (
+      <div className="Purchase-Summary-Main">
         <PriceSummary pricing={pricing} />
         <ItemDetails itemDetails={itemDetails} />
         <Promotion />
       </div>
+    ) : (
+      <div className="Purchase-Summary-Loader" />
     );
   }
 }
